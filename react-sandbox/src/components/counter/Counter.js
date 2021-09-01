d4a7b30ebf2e89e360f21afefbcf93ce732b395f
Christine Yoo <christinecho.s@gmail.com>
2021-09-01 16:47:05 -0700

    implemented star wars api for planets

--- react-sandbox/src/components/counter/Counter.js
+++ react-sandbox/src/components/counter/Counter.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [word, setWord] = useState('hello');
+  const [planet, setPlanet] = useState({});
+
+  const changeWord = () => {
+      if (word === 'hello') setWord('goodbye');
+      else setWord('hello');
+  }
+
+  //second parameter is an array that holds a value. If that value is changed, then the useEffect hook is run.
+  //if you pass in [], useEffect runs only ONCE (onMount) because [] never changes.
+  //summary: when any values in the second parameter are changed, the useEffect gets called
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/planets/${count}`)
+        .then(response => response.json())
+        .then(json => setPlanet(json))
+        .catch(err => console.log('an error occurred'))
+  }, [count]);
+
+//   useEffect(() => {
+//     fetch(`https://swapi.dev/api/people/${count}`)
+//         .then(response => response.json())
+//         .then(json => setPlanet(json))
+//         .catch(err => console.log('an error occurred'))
+//   }, [count])
 
     return ( <>
         <h1>Counter</h1>
@@ -15,6 +39,14 @@ const Counter = () => {
         <button onClick={() => setCount(0)}>
           Reset
         </button>
+        <p>{word}</p>
+        <button onClick={() => changeWord()}>Click me</button>
+        <h1>
+            Star Wars Planet: {count}
+        </h1>
+        <h2>{planet.name}</h2>
+        <h3>Climate: {planet.climate}</h3>
+        <h3>Terrain: {planet.terrain}</h3>
       </> );
 }
  
--- react-sandbox/src/components/landing/Landing.js
+++ react-sandbox/src/components/landing/Landing.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Landing = () => {
-  return <h1>The best landing page ever</h1>;
+  return (
+    <>
+      <h1>The best landing page ever</h1>
+      <h2>Star wars</h2>
+    </>
+  );
 };
 
 export default Landing;
