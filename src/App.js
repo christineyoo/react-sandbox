31f63cdd0494f63034d91507a8429db00ab344ee
Christine Yoo <christinecho.s@gmail.com>
2021-09-08 21:06:13 -0700

    added css bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Counter from './components/counter/Counter';
+import Counter from './components/counter/counter.jsx';
 import Landing from './components/landing/Landing';
 import NavBar from './components/navBar/NavBar';
+import ShoppingCounter from './components/shoppingCounter'
 
 function App() {
 
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <Switch>
         <Route exact path='/counter' component={Counter}/>
-        <Route exact path='/' component={Landing} />
+        <Route exact path='/' component={ShoppingCounter} />
       </Switch>
     </>
   );
